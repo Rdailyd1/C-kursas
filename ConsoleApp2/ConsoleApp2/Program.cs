0b098a9405cedbfac143271c86a90f9c601cd90c
Rolandas Dailyde <rdailyd@gmail.com>
2019-04-27 12:35:16 +0300

    Programa plotas

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Rectangle
+    {
+        //master variables
+        private double lenth;
+        private double width;
+
+        public void Acceptdetails()
+        {
+            lenth = 4.5;
+            width = 3.5;
+        }
+        public double GetArea()
+        {
+            return lenth * width;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Lenth: {0}", lenth);
+            Console.WriteLine("Width: {0}", width);
+            Console.WriteLine("Area: {0}", GetArea());
+        }
+    }
+    class Program
+    { 
+            static void Main(string[] args)
+    {
+        Rectangle r = new Rectangle();
+        r.Acceptdetails();
+        r.Display();
+        Console.ReadLine();
+    }
+
+    }
+}
