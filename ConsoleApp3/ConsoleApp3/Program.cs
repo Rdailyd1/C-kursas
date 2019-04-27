566936c2c777fd077607dfca9d1805769c1d7200
Rolandas Dailyde <rdailyd@gmail.com>
2019-04-27 12:56:04 +0300

    Vardas

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string vardas = "";
+            while (vardas != "stop")
+            {
+                Console.WriteLine("Įveskite savo vardą:");
+                vardas = Console.ReadLine();
+                Console.WriteLine("Sveiki {0} [Enter - testi]", vardas);
+
+            }
+            Console.Clear();
+            Console.WriteLine("Ate....");
+            Console.ReadLine();
+        }
+    }
+}
