16477659d8b38c05a5a256ba8be045d42cffd33d
Rolandas Dailyde <rdailyd@gmail.com>
2019-04-27 13:22:46 +0300

    Dalyba

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main()
+        {
+            int pirm = 24;
+            int antr = 5;
+            double dal = pirm / antr;
+
+            Console.WriteLine("Rezultatas: {0}", dal );
+            Console.ReadLine();
+        }
+    }
+}
