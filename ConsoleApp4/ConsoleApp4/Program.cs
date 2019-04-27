33de5c89573de57cae8f90abb04b12f18b06c545
Rolandas Dailyde <rdailyd@gmail.com>
2019-04-27 13:13:05 +0300

    Spalva

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string spalva = "1";
+            while (spalva != "2")
+            {
+                Console.WriteLine("Kokia jūsų mėgstama spalva:");
+                spalva = Console.ReadLine();
+                Console.WriteLine("Mano irgi");
+
+            }
+            Console.Clear();
+            //Console.WriteLine("Mano irgi");
+            Console.ReadLine();
+        }
+    }
+}
