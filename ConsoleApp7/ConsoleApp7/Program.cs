5ebcc672da1c376bd15d278791c06405eb8c9258
Rolandas Dailyde <rdailyd@gmail.com>
2019-04-27 13:27:06 +0300

    Veiksmai

---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main()
+        {
+            int reiksme1 = -6 + 3 * 5;
+            int reiksme2 = (13-2)*7;
+            double reiksme3 =(5+-2) + (20/10);
+
+            Console.WriteLine("Reikšmė 1: {0}", reiksme1);
+            Console.WriteLine("Reikšmė 2: {0}", reiksme2);
+            Console.WriteLine("Reikšmė 3: {0}", reiksme3);
+            Console.ReadLine();
+        }
+    }
+}
