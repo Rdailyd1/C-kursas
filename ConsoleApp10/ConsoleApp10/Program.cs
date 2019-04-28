3f53525bc2c6a5cf10b793c169e2f1cec66947c1
Rolandas Dailyde <rdailyd@gmail.com>
2019-04-28 13:49:48 +0300

    Vidurkis

---
+++ ConsoleApp10/ConsoleApp10/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class vidurkis
+    {
+
+        static void Main(string[] args)
+        {
+            int skaicius = 1;
+            int indeksas = -1;
+            int suma = 0;
+            double vidurkis = 0;
+            Console.WriteLine("Įveskite skaičius, sekos pabaiga skaičius 0");
+            while (skaicius != 0)
+            {
+                Console.WriteLine("Įveskite skaičių:");
+                skaicius = Convert.ToInt16(Console.ReadLine());
+                suma = suma + skaicius;
+                indeksas = indeksas + 1 ;
+                
+            }
+            //Console.Clear();
+            vidurkis = suma / indeksas;
+            Console.WriteLine("Vidurkis: {0}", vidurkis);
+            Console.ReadLine();
+        }
+    }
+}
