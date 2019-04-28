ec623113e3288a436bc9762ebbe71769ef855226
Rolandas Dailyde <rdailyd@gmail.com>
2019-04-28 19:37:44 +0300

    Stačiakampis

---
+++ ConsoleApp12/ConsoleApp12/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class staciakampis
+    {
+
+        static void Main(string[] args)
+        {
+            string skaicius = "";
+           
+            Console.WriteLine("Įveskite skaičių :");
+            skaicius = Console.ReadLine();
+            Console.WriteLine("{0} {1} {2}", skaicius, skaicius, skaicius);
+            Console.WriteLine("{0} {1} {2}", skaicius, " ", skaicius);
+            Console.WriteLine("{0} {1} {2}", skaicius, " ", skaicius);
+            Console.WriteLine("{0} {1} {2}", skaicius, " ", skaicius);
+            Console.WriteLine("{0} {1} {2}", skaicius, skaicius, skaicius);
+
+            Console.ReadLine();
+        }
+    }
+}
