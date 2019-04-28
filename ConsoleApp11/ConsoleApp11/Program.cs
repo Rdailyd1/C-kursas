47a0678d97f1fd57506ce1784be5b3f3e40ed457
Rolandas Dailyde <rdailyd@gmail.com>
2019-04-28 14:46:00 +0300

    Trys skaičiai

---
+++ ConsoleApp11/ConsoleApp11/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class trys_skaiciai
+    {
+
+        static void Main(string[] args)
+        {
+            int skaicius1 = 0;
+            int skaicius2 = 0;
+            int skaicius3 = 0;
+
+            Console.WriteLine("Įveskite skaičių A:");
+            skaicius1 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Įveskite skaičių B:");
+            skaicius2 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Įveskite skaičių C:");
+            skaicius3 = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("("+skaicius1 + "+"+skaicius2+")"+"*"+skaicius3+"="+(skaicius1+skaicius2)*skaicius3);
+            Console.WriteLine(skaicius1 + "*" + skaicius3 + "+" + skaicius2 + "*"+skaicius3 + "=" + ((skaicius1 * skaicius3) + (skaicius2*skaicius3)));
+
+            Console.ReadLine();
+        }
+    }
+}
