063dbaf064a925f854cce77624574b4cc5112657
Rolandas Dailyde <rdailyd@gmail.com>
2019-04-29 20:51:53 +0300

    Ar yra tekstas.

---
+++ ConsoleApp22/ConsoleApp22/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class trys_skaiciai
+    {
+
+        static void Main(string[] args)
+        {
+            string tekstas = "";
+
+            Console.WriteLine("Įveskite tekstą:");
+            tekstas = Console.ReadLine();
+            if (tekstas != "")
+                Console.WriteLine("True");
+            else Console.WriteLine("False");
+            Console.ReadLine();
+        }
+    }
+}
