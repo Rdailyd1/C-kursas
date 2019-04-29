1f1c7567dc63b1711e2a13c43991de7691e16512
Rolandas Dailyde <rdailyd@gmail.com>
2019-04-29 19:07:06 +0300

    3-jų sk. vidurkis

--- ConsoleApp16/ConsoleApp16/Program.cs
+++ ConsoleApp16/ConsoleApp16/Program.cs
@@ -13,20 +13,18 @@ namespace ConsoleApp3
         {
             int skaicius1 = 0;
             int skaicius2 = 0;
+            int skaicius3 = 0;
 
             Console.WriteLine("Įveskite skaičių 1:");
             skaicius1 = Convert.ToInt16(Console.ReadLine());
             Console.WriteLine("Įveskite skaičių 2:");
             skaicius2 = Convert.ToInt16(Console.ReadLine());
-            int suma = skaicius1 + skaicius2;
-            int skirtumas = skaicius1 - skaicius2;
-            int sandauga = skaicius1 * skaicius2;
-            decimal dalyba = skaicius1 / skaicius2;
-            Console.WriteLine("Suma: {0}", suma);
-            Console.WriteLine("skirtumas: {0}", skirtumas);
-            Console.WriteLine("sandauga: {0}", sandauga);
-            Console.WriteLine("dalyba: {0}", dalyba);
-
+            Console.WriteLine("Įveskite skaičių 3:");
+            skaicius3 = Convert.ToInt16(Console.ReadLine());
+            int suma = skaicius1 + skaicius2 + skaicius3;
+           
+            decimal vidurkis = suma/3;
+            Console.WriteLine("Vidurkis: {0}", vidurkis); 
 
             Console.ReadLine();
         }
