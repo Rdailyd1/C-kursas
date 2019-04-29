c33c980d780843f0dfe36d95ddac9edf658db8b8
Rolandas Dailyde <rdailyd@gmail.com>
2019-04-29 20:56:06 +0300

    Ar mažiau už 0.

---
+++ ConsoleApp23/ConsoleApp23/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class trys_skaiciai
+    {
+
+        static void Main(string[] args)
+        {
+            int skaicius = 0;
+
+            Console.WriteLine("Įveskite skaiciu:");
+            skaicius = Convert.ToInt16(Console.ReadLine());
+            if (skaicius < 0)
+                Console.WriteLine("True");
+            else Console.WriteLine("False");
+            Console.ReadLine();
+        }
+    }
+}
+
