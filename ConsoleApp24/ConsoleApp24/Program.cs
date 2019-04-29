b09cf7691a2ac1991848b7e058bc63c404693108
Rolandas Dailyde <rdailyd@gmail.com>
2019-04-29 21:02:53 +0300

    Viena eilutė.

---
+++ ConsoleApp24/ConsoleApp24/Program.cs
@@ -0,0 +1,27 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class vardas_pavarde
+    {
+
+        static void Main(string[] args)
+        {
+            string vardas = "";
+            string pavarde = "";
+
+            Console.WriteLine("Įveskite varda:");
+            vardas = Console.ReadLine();
+            Console.WriteLine("Įveskite pavarde:");
+            pavarde = Console.ReadLine();
+       
+            Console.Write(vardas + " " + pavarde);
+           
+            Console.ReadLine();
+        }
+    }
+}
