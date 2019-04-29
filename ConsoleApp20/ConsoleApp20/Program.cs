e7fe2534536b1ce631dc9ad1a5a4ec42e358338b
Rolandas Dailyde <rdailyd@gmail.com>
2019-04-29 20:40:46 +0300

    Sulyginimas

---
+++ ConsoleApp20/ConsoleApp20/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class trys_skaiciai
+    {
+
+        static void Main(string[] args)
+        {
+            int skaicius1 = 0;
+            int skaicius2 = 0;
+            bool sulyginimas = true;
+
+            Console.WriteLine("Įveskite skaičių A:");
+            skaicius1 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Įveskite skaičių B:");
+            skaicius2 = Convert.ToInt16(Console.ReadLine());
+            if (skaicius1 == skaicius2)
+                Console.WriteLine("True");
+            else Console.WriteLine("False");
+
+            Console.ReadLine();
+        }
+    }
+}
