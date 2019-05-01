5f5f8e276e8e768f369616b251d3e348698eee06
Rolandas Dailyde <rdailyd@gmail.com>
2019-05-01 19:00:36 +0300

    Liekana

---
+++ NM2_6/NM2_6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NM2_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int skaicius = 0;
+            int pirmas = 0;
+            int antras = 0;
+            int suma = 0;
+            Console.WriteLine("Įveskite 2 ženklų skaičių:");
+            skaicius = Convert.ToInt32(Console.ReadLine());
+            pirmas = skaicius / 10;
+            antras = skaicius % 10;
+            suma = pirmas + antras;
+            Console.WriteLine("Skaiciu suma: {0}", suma);
+            Console.ReadLine();
+        }
+    }
+}
