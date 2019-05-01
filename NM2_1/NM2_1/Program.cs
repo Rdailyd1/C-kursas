2a688ae3543b35538534b6380474bc81925ab6cc
Rolandas Dailyde <rdailyd@gmail.com>
2019-05-01 16:28:49 +0300

    Didžiosios raidės

---
+++ NM2_1/NM2_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class adresas
+    {
+
+        static void Main(string[] args)
+        {
+            string vardas = "";
+            string pavarde = "";
+
+            Console.WriteLine("Įveskite vardą:");
+            vardas = Console.ReadLine();
+            Console.WriteLine("Įveskite pavardę:");
+            pavarde = Console.ReadLine();
+            string didziosios = (vardas + "_" + pavarde).ToUpper();
+            Console.WriteLine("Vardas pavardė {0}", didziosios);
+            Console.ReadLine();
+        }
+    }
+}
