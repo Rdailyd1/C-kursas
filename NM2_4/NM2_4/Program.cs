0f16ee7b0c2f8fd2b3ade90c83592958eb05a77b
Rolandas Dailyde <rdailyd@gmail.com>
2019-05-01 18:31:48 +0300

    Sekundės

---
+++ NM2_4/NM2_4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sekundes
+{
+    class sekundes
+    {
+
+        static void Main(string[] args)
+        {
+            int skaicius;
+            Console.WriteLine("Įveskite skaičių (sekundes):");
+            skaicius = Convert.ToInt16(Console.ReadLine());
+            TimeSpan laikas = TimeSpan.FromSeconds(skaicius);
+            DateTime dateTime = DateTime.Today.Add(laikas);
+            string val_min_sek = dateTime.ToString("hh:mm:ss");
+            Console.WriteLine("Valandos: minutes: sekundes - {0}", val_min_sek);
+            Console.ReadLine();
+        }
+    }
+}
