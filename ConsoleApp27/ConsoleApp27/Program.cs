3e535cea96718ddc33ac2f553c3acefc1f873020
Rolandas Dailyde <rdailyd@gmail.com>
2019-05-02 18:54:24 +0300

    Double - float

---
+++ ConsoleApp27/ConsoleApp27/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float skaiciusFloat = 1f / 3;
+            double skaiciusDouble = 1d / 3;
+            if(skaiciusFloat==skaiciusDouble)
+            {
+                Console.WriteLine("skaiciusFloat {0} YRA skaiciusDouble {1}", skaiciusFloat, skaiciusDouble);
+            }
+            else
+            {
+                Console.WriteLine("skaiciusFloat {0} NE LYGUS skaiciusDouble {1}", skaiciusFloat, skaiciusDouble);
+            }
+            Console.ReadLine();
+        }
+    }
+}
