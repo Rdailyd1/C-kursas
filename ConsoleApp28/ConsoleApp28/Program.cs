2214faaac09d81bee9f0c5bb272f754190dea060
Rolandas Dailyde <rdailyd@gmail.com>
2019-05-02 19:08:29 +0300

    Double, float

---
+++ ConsoleApp28/ConsoleApp28/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal skaicius1 = 0.1m * 10;
+            decimal skaicius2 = 0;
+            for (int ctr = 0; ctr < 10; ctr++)
+
+            {
+                skaicius2 += 0.1m;
+            }
+            Console.WriteLine("{0:N}={1:N}:{2}", skaicius1, skaicius2, skaicius1 == skaicius2);
+            Console.ReadLine();
+        }
+    }
+}
