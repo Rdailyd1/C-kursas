90ea83bc1855067e98f2fa824c4ff19f1cab5e8f
Rolandas Dailyde <rdailyd@gmail.com>
2019-05-02 19:23:13 +0300

    sbyte

---
+++ ConsoleApp30/ConsoleApp30/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte a = 126;
+            Console.WriteLine(a);
+            a++;
+            Console.WriteLine(a);
+           
+            a++;
+            Console.WriteLine(a);
+            a++;
+            Console.WriteLine(a);
+
+            Console.ReadLine();
+        }
+    }
+}
