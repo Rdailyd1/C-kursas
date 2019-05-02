a5294bcc1f05b3c7e591134166937426e0716069
Rolandas Dailyde <rdailyd@gmail.com>
2019-05-02 20:10:04 +0300

    Simboliai, trikampis

---
+++ ConsoleApp32/ConsoleApp32/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 1234.7;
+            int a;
+            a = (int)x;
+            Console.WriteLine(x);
+            Console.WriteLine(a);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp33/ConsoleApp33/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class staciakampis
+    {
+
+        static void Main(string[] args)
+        {
+            string simbolis = "";
+
+            Console.WriteLine("Įveskite simboli :");
+           simbolis = Console.ReadLine();
+            Console.WriteLine("{0} ", simbolis);
+            Console.WriteLine("{0} {0} ", simbolis, simbolis);
+            Console.WriteLine("{0} {0} {0}", simbolis, simbolis, simbolis);
+           
+            Console.ReadLine();
+        }
+    }
+}
