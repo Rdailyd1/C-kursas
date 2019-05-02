746bb7235385db4e24f824ac0231a50a05f34a10
Rolandas Dailyde <rdailyd@gmail.com>
2019-05-02 20:25:12 +0300

    Vardas, slaptažodis

---
+++ ConsoleApp34/ConsoleApp34/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class staciakampis
+    {
+
+        static void Main(string[] args)
+        {
+            string tikras_vardas = "Rolandas";
+            string tikras_slapt = "45rd";
+            string vardas = "";
+            string slaptazodis = "";
+           
+           
+            for (int index = 0; index < 3; index++)
+            {
+                Console.WriteLine("Įveskite varda :");
+                vardas = Console.ReadLine();
+                Console.WriteLine("Įveskite slaptazodi :");
+                slaptazodis = Console.ReadLine();
+                if (vardas == tikras_vardas && slaptazodis == tikras_slapt)
+                {
+                    Console.WriteLine("Teisingai");
+                    break;
+                }
+                else
+                {
+                 
+                    if (index == 2)
+                    {
+                        Console.WriteLine("Suklydote 3 kartus");
+                        break;
+                    }
+                    else {
+                        Console.WriteLine("Neteisingai, bandykite dar karta");
+                    }
+
+                }
+            }
+           
+
+            Console.ReadLine();
+        }
+    }
+}
