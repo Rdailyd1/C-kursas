fedbbe5eb22d951243d051bfa28941645c6f8e8d
Rolandas Dailyde <rdailyd@gmail.com>
2019-05-02 19:20:41 +0300

    byte

---
+++ ConsoleApp29/ConsoleApp29/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte a = 0;
+            Console.WriteLine(a);
+            a++;
+            Console.WriteLine(a);
+            a = 254;
+            Console.WriteLine(a);
+            a++;
+            Console.WriteLine(a);
+            a=(byte)(a+(byte)10);
+            Console.WriteLine(a);
+            
+            Console.ReadLine();
+        }
+    }
+}
