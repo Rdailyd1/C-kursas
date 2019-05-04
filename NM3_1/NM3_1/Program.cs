ee93f9ca12f4fd6739e8b5a600d896476fe29879
Rolandas Dailyde <rdailyd@gmail.com>
2019-05-04 19:14:12 +0300

    Skaičių lyginimas

---
+++ NM3_1/NM3_1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int sk1;
+            int sk2;
+
+            Console.WriteLine("Iveskite pirma skaiciu:");
+            sk1 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Iveskite antra skaiciu:");
+            sk2 = Convert.ToInt16(Console.ReadLine());
+            if (sk1 == sk2)
+            {
+                Console.WriteLine("Skaiciai vienodi");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Skaiciai skiriasi");
+                Console.ReadLine();
+            }
+        }
+    }
+}
