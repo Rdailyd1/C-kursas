0789a00e6347bea67141662cdb703d9bb21b4235
Rolandas Dailyde <rdailyd@gmail.com>
2019-05-04 19:35:28 +0300

    Lyginis, nelyginis, teigiamas, neigiamas

---
+++ MN3_3/MN3_3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            double sk1;
+            Console.Write("Iveskite skaiciu:");
+            sk1 = Convert.ToDouble(Console.ReadLine());
+            if (sk1 % 2 == 0)
+            {
+                Console.Write("Skaicius lyginis");
+            }
+            else
+            {
+                Console.Write("Skaicius nelyginis");
+            }
+            if (sk1 > 0)
+            {
+                Console.Write(", teigiamas");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.Write(", neigiamas");
+                Console.ReadLine();
+            }
+        }
+    }
+}
