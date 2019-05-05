d25a4f2ca20c65fab0442c3ac9b45a44f2f39e9e
Rolandas Dailyde <rdailyd@gmail.com>
2019-05-05 21:07:21 +0300

    Mokesčiai

---
+++ MN3_10/MN3_10/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            decimal mokestis = 0;
+            Console.WriteLine("Iveskite pajamas");
+            int pajamos;
+            if (int.TryParse(Console.ReadLine(), out pajamos))
+            {
+
+                if (pajamos > 100000)
+                {
+                    mokestis = (decimal)pajamos * 35 / 100;
+                }
+                else if (pajamos > 75000)
+                {
+                    mokestis = (decimal)pajamos * 20 / 100;
+                }
+                else if (pajamos >= 40000)
+                {
+                    mokestis = (decimal)pajamos * 15 / 100;
+                }
+                if (mokestis == 0)
+                {
+                    Console.WriteLine("Jums nereikia moketi mokesciu");
+                }
+                else
+                {
+                    Console.WriteLine("Mokestis: {0}", mokestis);
+                }
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Ivestas blogas skaicius");
+                Console.ReadLine();
+            }
+        }
+    }
+}
