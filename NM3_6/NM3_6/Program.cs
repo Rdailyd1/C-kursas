30e7b582d96061eb552d07072d5f41cea45fda04
Rolandas Dailyde <rdailyd@gmail.com>
2019-05-05 18:02:34 +0300

    Trikampis

---
+++ NM3_6/NM3_6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class trikampis
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Įveskite skaičių 1:");
+            int skaicius1 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Įveskite skaičių 2:");
+            int skaicius2 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Įveskite skaičių 3:");
+            int skaicius3 = Convert.ToInt16(Console.ReadLine());
+            int suma = skaicius1 + skaicius2 + skaicius3;
+            if (suma == 180)
+            {
+                Console.WriteLine("Gali buti sudarytas trikampis");
+            }
+            else
+            {
+                Console.WriteLine("Negali buti sudarytas trikampis, nes kampu suma nelygi 180");
+            }
+            Console.ReadLine();
+        }
+    }
+}
