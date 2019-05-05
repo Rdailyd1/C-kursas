443c3b8c5e9e5cb148051622a998bf6f0f0c2802
Rolandas Dailyde <rdailyd@gmail.com>
2019-05-05 18:31:14 +0300

    Papildymas dėl skaičių tikrinimo

--- NM3_8/NM3_8/Program.cs
+++ NM3_8/NM3_8/Program.cs
@@ -13,30 +13,39 @@ namespace ConsoleApp3
         {
 
             Console.WriteLine("Iveskite skaiciu nuo 1 iki 7");
-            int diena = Convert.ToByte(Console.ReadLine());
-            switch (diena)
+            string diena = Console.ReadLine();
+            bool res = Int32.TryParse(diena, out int val);
+            if (res)
             {
-                case 1:
-                    Console.WriteLine("Pirmadienis");
-                    break;
-                case 2:
-                    Console.WriteLine("Antradienis");
-                    break;
-                case 3:
-                    Console.WriteLine("Treciadienis");
-                    break;
-                case 4:
-                    Console.WriteLine("Ketvirtadienis");
-                    break;
-                case 5:
-                    Console.WriteLine("Penktadienis");
-                    break;
-                case 6:
-                    Console.WriteLine("Sestadienis");
-                    break;
-                case 7:
-                    Console.WriteLine("Sekmadienis");
-                    break;
+                int sav_diena = Convert.ToInt16(diena);
+                switch (sav_diena)
+                {
+                    case 1:
+                        Console.WriteLine("Pirmadienis");
+                        break;
+                    case 2:
+                        Console.WriteLine("Antradienis");
+                        break;
+                    case 3:
+                        Console.WriteLine("Treciadienis");
+                        break;
+                    case 4:
+                        Console.WriteLine("Ketvirtadienis");
+                        break;
+                    case 5:
+                        Console.WriteLine("Penktadienis");
+                        break;
+                    case 6:
+                        Console.WriteLine("Sestadienis");
+                        break;
+                    case 7:
+                        Console.WriteLine("Sekmadienis");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ivesta klaidinga reiksme");
             }
             Console.ReadLine();
         }
