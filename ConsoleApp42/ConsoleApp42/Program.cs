1360fb3ff86b0d3fcaefb630d029fc0629885ddb
Rolandas Dailyde <rdailyd@gmail.com>
2019-05-06 19:15:52 +0300

    Progresas

---
+++ ConsoleApp42/ConsoleApp42/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite skaiciu");
+            int sk = int.Parse(Console.ReadLine());
+            for (int i = 0; i < sk; i++)
+            {
+                Thread.Sleep(50); // milisekundes
+                Console.Write("#");
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
