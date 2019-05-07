db83f1d5d73c4471fb1d041f64219b122b60d635
Rolandas Dailyde <rdailyd@gmail.com>
2019-05-07 09:10:28 +0300

    Papildymas dėl procentinio pokyčio atvaizdavimo

--- ConsoleApp42/ConsoleApp42/Program.cs
+++ ConsoleApp42/ConsoleApp42/Program.cs
@@ -9,14 +9,21 @@ namespace ConsoleApp42
 {
     class Program
     {
+        protected static int origRow;
         static void Main(string[] args)
         {
             Console.WriteLine("Iveskite skaiciu");
-            int sk = int.Parse(Console.ReadLine());
-            for (int i = 0; i < sk; i++)
+            int sk = Convert.ToInt16(Console.ReadLine());
+            decimal proc = 0;
+            for (int i = 1; i <= sk; i++)
             {
-                Console.SetCursorPosition(1, 1);
+                origRow = Console.CursorTop;
+                Console.SetCursorPosition(1, 2);
+                proc = (decimal)i / sk * 100;
+                Thread.Sleep(50);
+                Console.Write(Convert.ToString(Convert.ToInt16(proc)) + "%");
                 Thread.Sleep(50); // milisekundes
+                Console.SetCursorPosition(i, 3);
                 Console.Write("#");
             }
             Console.WriteLine();
