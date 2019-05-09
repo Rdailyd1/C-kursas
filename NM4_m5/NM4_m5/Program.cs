d675da2d0e298584922558a6ab47ac65c0e7031e
Rolandas Dailyde <rdailyd@gmail.com>
2019-05-09 21:52:53 +0300

    Vardo raidės ir jų išdėstymas atvirkštine tvarka

---
+++ NM4_m5/NM4_m5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp44
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string vardas = "Rolandas";
+
+            for (int i = 0; i < vardas.Length; i++)
+            {
+                char current = vardas[i];
+                System.Console.Write(current);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Vardas atvirkstine tvarka: ");
+            for (int i = vardas.Length - 1; i >= 0; i--)
+            {
+                char current = vardas[i];
+                System.Console.Write(current);
+            }
+            Console.ReadLine();
+        }
+    }
+}
