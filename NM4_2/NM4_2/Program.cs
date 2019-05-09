2b1d1170ed615c5f5e0ec2945bce2d4e5bb60de0
Rolandas Dailyde <rdailyd@gmail.com>
2019-05-09 15:46:22 +0300

    Ciklo skaičiai

---
+++ NM4_2/NM4_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp42
+{
+    class Program
+    {
+        protected static int origRow;
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
