3f6ffa4e636bc32f2f5cea644c5bf429b95c2cda
Rolandas Dailyde <rdailyd@gmail.com>
2019-05-09 16:10:54 +0300

    Trikampis, ciklas cikle

---
+++ NM4_5/NM4_5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace NM4_5
+{
+    class Program
+    {
+        protected static int origRow;
+        static void Main(string[] args)
+        {
+         
+            for (int a = 4; a >= 1; a--)
+            {
+                for (int i = a; i <= 4; i++)
+                {
+                    
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
