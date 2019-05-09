4d9dd2279aac8f6f42265ac14a8ce5e068800e82
Rolandas Dailyde <rdailyd@gmail.com>
2019-05-09 21:45:56 +0300

    Teksto raidės ir jų išdėstymas atvirkštine tvarka

---
+++ NM4_m4/NM4_m4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp44
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string vardas = "pavadinimas";
+           
+            for (int i = 0; i < vardas.Length; i++)
+            {
+                char current = vardas[i];
+                System.Console.Write(current);
+            }
+            Console.WriteLine();
+            for (int i = vardas.Length-1; i >= 0; i--)
+            {
+                char current = vardas[i];
+                System.Console.Write(current);
+            }
+            Console.ReadLine();
+        }
+    }
+}
