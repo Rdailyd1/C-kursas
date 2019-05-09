526a59e43162f78fd5b2041e552acd625ca94fb5
Rolandas Dailyde <rdailyd@gmail.com>
2019-05-09 16:02:38 +0300

    Be liekanos

---
+++ NM4_4/NM4_4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp42
+{
+    class Program
+    {
+        protected static int origRow;
+        static void Main(string[] args)
+        {
+            int be_liekanos = 0;
+            Console.WriteLine("Be liekanos yra sie skaiciai: ");
+            for (int i = 0; i <= 100; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    be_liekanos = i;
+                    Console.WriteLine(be_liekanos);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
