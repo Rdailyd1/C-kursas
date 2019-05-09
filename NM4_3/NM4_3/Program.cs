5af07a374f6d33f3e0de51e2eeed194ec69f1bd6
Rolandas Dailyde <rdailyd@gmail.com>
2019-05-09 15:56:23 +0300

    Skaičių suma

---
+++ NM4_3/NM4_3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp42
+{
+    class Program
+    {
+        protected static int origRow;
+        static void Main(string[] args)
+        {
+            int suma = 0;
+            for (int i = 10; i <= 20; i++)
+            {
+                suma += i;
+            }
+            Console.WriteLine("Skaiciu nuo 10 ik 20 suma= {0}", suma);
+            Console.ReadLine();
+        }
+    }
+}
