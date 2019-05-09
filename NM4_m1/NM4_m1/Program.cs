f4d81456f0d6321c7c79f7a1ada79164aee3cdcd
Rolandas Dailyde <rdailyd@gmail.com>
2019-05-09 20:11:18 +0300

    5-ių elementų masyvas

---
+++ NM4_m1/NM4_m1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp44
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] masyvas1 = new int[5];
+            Random rnd1 = new Random();
+            for (int i = 0; i < masyvas1.Length; i++)
+            {
+                masyvas1[i] = rnd1.Next(1, 100);
+                Console.Write("{0} ", masyvas1[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
