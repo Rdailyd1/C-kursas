ddf65303eb75883c1e4bec53f8f045c4f652ca2a
Rolandas Dailyde <rdailyd@gmail.com>
2019-05-09 20:22:12 +0300

    Papildymas su Foreach

---
+++ NM4_m2/NM4_m2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp44
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] masyvas1 = new int[5];
+            Random rnd1 = new Random();
+            for (int i = 0; i < masyvas1.Length; i++)
+            {
+                masyvas1[i] = rnd1.Next(1, 100);
+            }
+            foreach (var skaicius in masyvas1)
+            {
+                Console.Write("{0} ", skaicius);
+            }
+            Console.ReadLine();
+        }
+    }
+}
