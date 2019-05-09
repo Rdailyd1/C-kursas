eb77dc9bdb722c796f80f3b45ba82be082088b61
Rolandas Dailyde <rdailyd@gmail.com>
2019-05-09 15:44:14 +0300

    Vardas

---
+++ NM4_1/NM4_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp42
+{
+    class Program
+    {
+        protected static int origRow;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite savo varda: ");
+            string vardas = Console.ReadLine();
+            for (int i = 1; i <= 10; i++)
+            {              
+                Console.WriteLine(vardas); 
+            }
+            Console.ReadLine();
+        }
+    }
+}
