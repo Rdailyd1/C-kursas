444f08196fe6b1435fef9e309409d10c7943e7fc
Rolandas Dailyde <rdailyd@gmail.com>
2019-05-10 19:52:53 +0300

    winforms programa

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (tbvardas.Text == "")
+            {
+                MessageBox.Show("Vardas negali buti tuscias!");
+            }
+            tboutput.Clear();
+            tboutput.Text = String.Format("Sveikas {0}", tbvardas.Text);
+            tboutput.AppendText(Environment.NewLine);
+            tboutput.AppendText = String.Format("Malonu tave matyt");
+        }    
+    }
+}
