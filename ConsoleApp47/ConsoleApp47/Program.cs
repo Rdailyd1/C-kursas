e296fb07460a6dd71b000f5a6a1d7e55f462ac2d
Rolandas Dailyde <rdailyd@gmail.com>
2019-05-13 20:29:41 +0300

    Palindromas

--- ConsoleApp47/ConsoleApp47/Program.cs
+++ ConsoleApp47/ConsoleApp47/Program.cs
@@ -4,22 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lesson06_04.rekursija
+namespace palindromas
 {
     class Program
     {
-        public static string vardas(string name)
+        public static string palindromas(string tekstas)
         {
-            Console.WriteLine(name);
-            return name;
+            char[] charArray = tekstas.ToCharArray();
+            Array.Reverse(charArray);
+            string atvirkstinis = new string(charArray);
+            if (tekstas == atvirkstinis)
+            {
+                Console.WriteLine(atvirkstinis);
+                Console.WriteLine("Tai yra palindromas");
+            }
+            else
+            {
+                Console.WriteLine("Tai nera palindromas");
+            }
+            return atvirkstinis;
         }
 
         static void Main(string[] args)
         {
-            string name = "Rolandas";
+            Console.WriteLine("Iveskite teksta");
 
+            string tekstas = Console.ReadLine();
 
-            vardas(name);
+            palindromas(tekstas);
 
             Console.ReadKey();
         }
---
+++ ConsoleApp48/ConsoleApp48/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace palindromas
+{
+    class Program
+    {
+        public static int fibonatis(int skaicius)
+        {
+            int ankstesnis = 0;
+            int sum = 0;
+            for (int i = 1; i <= skaicius; i++)
+            {
+                ankstesnis = i-1;
+                sum += ankstesnis;
+                Console.Write(sum + " ");
+            }
+            return skaicius;
+        }
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Iveskite skaiciu");
+
+                int skaicius = Convert.ToInt32(Console.ReadLine());
+
+                fibonatis(skaicius);
+
+                Console.ReadKey();
+            }
+        }
+    }
+
