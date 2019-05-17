b04a3169c1554c86e856bb5da20b02f26c48a2ac
Rolandas Dailyde <rdailyd@gmail.com>
2019-05-17 20:15:35 +0300

    Skaičių nuo iki suma ir tarpinė suma

---
+++ ConsoleApp54/ConsoleApp54/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace skaiciu_suma
+{
+    class Program
+    {
+        public static int nuo_iki(int nuo, int iki)
+        {
+            int suma = 0;
+            for (int i = nuo; i <= iki; i++)
+            {
+                suma += i;
+            }
+            return suma;
+        }
+        public static int nuo_iki(int nuo, int iki, bool rodytiTarpineSuma)
+        {
+            int suma = 0;
+            Console.WriteLine("Tarpine suma");
+            for (int i = nuo; i <= iki; i++)
+            {
+                suma += i;
+                Console.WriteLine(suma);
+            }
+            return suma;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite skaiciu NUO");
+            int nuo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Iveskite skaiciu IKI");
+            int iki = Convert.ToInt32(Console.ReadLine());
+            int rez =nuo_iki(nuo, iki);
+            Console.WriteLine("Rezultatas {0}", rez);
+            nuo_iki(nuo, iki, true);
+            Console.ReadKey();
+        }
+    }
+}
+
