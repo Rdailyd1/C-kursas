c9b73f5b9d0843c1c4c75c28d94d2953f34928d0
Rolandas Dailyde <rdailyd@gmail.com>
2019-05-17 19:49:10 +0300

    Skaičiaus skaitmenų suma ir elementų kiekis

---
+++ ConsoleApp52/ConsoleApp52/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp52
+{
+    class Program
+    {
+        public static void skaitmenys(int skaicius)
+        {
+            int sum = 0;
+            int el = 0;
+            while (skaicius != 0)
+            {
+                sum += skaicius % 10;
+                skaicius /= 10;
+                el++;
+            }
+            Console.WriteLine("Skaitmenu suma {0}", sum);
+            Console.WriteLine("Elementu skaicius {0}", el);
+            Console.ReadKey();
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite skaiciu");
+            int skaicius = Convert.ToInt16(Console.ReadLine());
+            skaitmenys(skaicius);
+        }
+    }
+}
