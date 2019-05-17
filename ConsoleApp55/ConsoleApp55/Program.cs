9e69463c27811b42383665382b77ddd73b522b06
Rolandas Dailyde <rdailyd@gmail.com>
2019-05-17 21:36:39 +0300

    Vardo ir pavardės sujungimas

---
+++ ConsoleApp55/ConsoleApp55/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vardas_pavarde
+{
+    class Program
+    {
+        public static string sujungimas(string vardas, string pavarde)
+        {
+            string rez = vardas + ", " + pavarde;
+            return rez;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite varda");
+            string vardas = Console.ReadLine();
+            Console.WriteLine("Iveskite pavarde");
+            string pavarde = Console.ReadLine();
+            string sujungta = sujungimas(vardas, pavarde);
+            Console.WriteLine(sujungta);
+            Console.ReadKey();
+        }
+    }
+}
+
+
