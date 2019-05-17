35fd3abff3d04c7a77a01df92a31292078d2a880
Rolandas Dailyde <rdailyd@gmail.com>
2019-05-17 23:02:51 +0300

    Atvirkštinis skaičius

---
+++ NM6_8/NM6_8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NM6_8
+{
+    class Program
+    {
+        public static int atvirkstinis(string skaicius)
+        {
+            string rez;
+            int gr = 0;
+            char[] charArray = skaicius.ToCharArray();
+            Array.Reverse(charArray);
+            rez = new string(charArray);
+            gr = Int32.Parse(rez);
+            return gr;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite skaiciu");
+            int skaicius = int.Parse(Console.ReadLine());
+            Console.WriteLine("Atvirkstinis {0}", atvirkstinis(skaicius.ToString()));
+            Console.ReadKey();
+        }
+    }
+}
