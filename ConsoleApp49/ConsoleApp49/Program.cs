a377869841e0a42cc019313e6fd2eb8c5b84996f
Rolandas Dailyde <rdailyd@gmail.com>
2019-05-17 18:45:29 +0300

    Skaičiuoja tarpus tekste

---
+++ ConsoleApp49/ConsoleApp49/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace skaitmenu_suma
+{
+    class Program
+    {
+        public static int skaitmenys(int skaicius)
+        {
+            
+           string skait = skaicius.ToString();
+               foreach (string sk in skait)
+            {
+
+            }
+               
+            return skaicius;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite skaiciu");
+
+            int skaicius = Convert.ToInt32(Console.ReadLine());
+
+            skaitmenys(skaicius);
+
+            Console.ReadKey();
+        }
+    }
+}
+
