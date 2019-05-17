21bd282a628f5b9aac9406b124f55d23aa6f3d57
Rolandas Dailyde <rdailyd@gmail.com>
2019-05-17 19:58:29 +0300

    Kėlimas laipsniu

---
+++ ConsoleApp53/ConsoleApp53/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fibonacio_skaicius
+{
+    class Program
+    {
+        public static int kelimas_laipsniu(int skaicius, int laipsnis)
+        {
+            int rezultatas = 1;
+            for (int i = 0; i < laipsnis; i++)
+            {
+                rezultatas *= skaicius;
+            }
+
+            return rezultatas;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite skaiciu");
+            int skaicius = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Iveskite laipsni");
+            int laipsnis = Convert.ToInt32(Console.ReadLine());
+            int rez = kelimas_laipsniu(skaicius, laipsnis);
+            Console.WriteLine("Rezultatas {0}", rez);
+            Console.ReadKey();
+        }
+    }
+}
+
