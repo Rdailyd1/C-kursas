836a921910a6e9c92ac1899128fba1b32964751f
Rolandas Dailyde <rdailyd@gmail.com>
2019-05-17 22:46:18 +0300

    Masyvo elementų suma

---
+++ NM6_7b/NM6_7b/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NM6_7b
+{
+    class Program
+    {
+        public static decimal skaiciu_masyvas(decimal[] masyvas)
+        {
+            decimal sum = 0;
+            for (int v = 0; v < 5; v++)
+            {
+                sum += masyvas[v];
+            }
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            decimal[] sk_mas = new decimal[5];
+
+            sk_mas[0] = 3.645m;
+            sk_mas[1] = 6.2662m;
+            sk_mas[2] = 4.2145m;
+            sk_mas[3] = 7.532m;
+            sk_mas[4] = 9.255m;
+            decimal rez = Math.Round(skaiciu_masyvas(sk_mas),2);
+            Console.WriteLine("Masyvo elementu suma {0}", rez);
+            Console.ReadLine();
+        }
+    }
+}
+
