71c61fa66a5d88a2b92228eaccc84b3c00b178da
Rolandas Dailyde <rdailyd@gmail.com>
2019-05-17 22:36:26 +0300

    Metodo parametras skaičių masyvas

---
+++ NM6_7/NM6_7/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NM6_6
+{
+    class Program
+    {
+        public static void skaiciu_masyvas(decimal [] masyvas)
+        {
+            decimal elem = 0;
+            for (int v = 0; v < 5; v++)
+            {
+                elem = masyvas[v];
+                Console.Write(elem);
+                if (v < 4)
+                {
+                    Console.Write(", ");
+                }
+                else
+                {
+                    Console.Write(".");
+                }
+            }
+           
+            Console.ReadKey();
+        }
+        static void Main(string[] args)
+        {
+            decimal[] sk_mas = new decimal[5];
+           
+            sk_mas[0] = 5.6m;
+            sk_mas[1] = 3.2m;
+            sk_mas[2] = 4.4m;
+            sk_mas[3] = 7.3m;
+            sk_mas[4] = 2.2m;
+            skaiciu_masyvas(sk_mas);
+        }
+    }
+}
