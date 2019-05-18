024cc3e26a5e8210c24e127e2302d948f5eacac4
Rolandas Dailyde <rdailyd@gmail.com>
2019-05-18 10:29:36 +0300

    Daugiau, mažiau

---
+++ ConsoleApp56/ConsoleApp56/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp56
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int skaicius = rnd.Next(10);
+            int vat_sk = 0;
+            int s = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Iveskite savo skaiciu");
+                vat_sk = int.Parse(Console.ReadLine());
+                if (vat_sk > skaicius)
+                {
+                    Console.WriteLine("Maziau");
+                }
+                else if (vat_sk < skaicius)
+                {
+                    Console.WriteLine("Daugiau");
+                }
+                 else if (vat_sk == skaicius)
+                {
+                    Console.WriteLine("Atspejote");
+                    s = 1;
+                    break; 
+                }
+            }
+            if (s == 0)
+            {
+                Console.WriteLine("Neatspejote");
+            }
+            Console.ReadKey();
+        }
+    }
+}
